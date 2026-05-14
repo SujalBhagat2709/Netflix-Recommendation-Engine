25d810586ea8cd6e141441840c0133a336069b69
Sujal Bhagat <167618538+SujalBhagat2709@users.noreply.github.com>
2026-05-14 09:35:57 +0530

    Add files via upload

---
+++ movies.py
@@ -0,0 +1,26 @@
+movies = [
+    {
+        "title": "Inception",
+        "genre": "Sci-Fi",
+        "rating": 9
+    },
+    {
+        "title": "Interstellar",
+        "genre": "Sci-Fi",
+        "rating": 9
+    },
+    {
+        "title": "John Wick",
+        "genre": "Action",
+        "rating": 8
+    }
+]
+
+def get_movies():
+    
+    return movies
+
+
+if __name__ == "__main__":
+    
+    print(get_movies())
